c45317653798f6318db7a9a1a5eeb8646bc34a78
Liz Abinante <eabinante@gmail.com>
2014-11-11 20:51:12 -0800

    create user route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::UsersController < ActionController::API
   # POST to create a new user
   def create
+    @user = User.new(user_params)
+    @user if @user.save
   end
 
   # GET to request a user
@@ -10,5 +12,15 @@ class Api::V1::UsersController < ActionController::API
 
   # PUT to update a user
   def update
+  end
+
+  private
+  def user_params
+    {
+      email: params[:email],
+      password: params[:password],
+      first_name: params[:first_name],
+      last_name: params[:last_name]
+    }
   end
 end
