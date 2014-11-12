a70b61bcd79a9875764f7b5478864ddf0b93de5c
Liz Abinante <eabinante@gmail.com>
2014-11-11 21:26:08 -0800

    validations for length, email, etc

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates_presence_of :email, :password, :password_digest
+  validates_uniqueness_of :email
+  validates_length_of :password, within: 11..254
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
