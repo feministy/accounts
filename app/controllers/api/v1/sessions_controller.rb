16566dafce8671a5b9307c75f01bb28293ba8a23
Liz Abinante <eabinante@gmail.com>
2014-11-11 21:32:53 -0800

    login with unauthorized

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::SessionsController < ActionController::API
   # GET login/auth
   def login
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      # create_session
+      render json: UserSerializer.new(@user)
+    else
+      render json: { errors: { messages: 'Invalid email or password.' } }, status: :unauthorized
+    end
   end
 
   # DESTROY existing sessions
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users,   only: [:create, :show, :update]
-      get       'login',  to: 'sessions#login'
-      delete    'logout', to: 'sessions#logout'
+      get       '/login',  to: 'sessions#login'
+      delete    '/logout', to: 'sessions#logout'
     end
   end
 end
