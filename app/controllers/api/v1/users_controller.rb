a66918b6bc8376ce49a6db7d73adf7945088d33e
Liz Abinante <eabinante@gmail.com>
2014-11-11 21:26:22 -0800

    user save errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,17 @@ class Api::V1::UsersController < ActionController::API
   # POST to create a new user
   def create
     @user = User.new(user_params)
-    @user if @user.save
+    if @user.save
+      render json: UserSerializer.new(@user)
+    else
+      render json: { errors: @user.errors }
+    end
   end
 
   # GET to request a user
   def show
     @user = User.find(params[:id])
+    render json: UserSerializer.new(@user) if @user
   end
 
   # PUT to update a user
